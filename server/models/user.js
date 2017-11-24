99c372769b7d41a4fbcf49906929e8aaab14ed13
Namiq Haciyev <namiq886@gmail.com>
2017-11-24 16:26:10 +0400

    Hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
 const {
     isEmail
 } = require("validator");
@@ -75,6 +76,19 @@ UserSchema.statics.findByToken = function (token) {
 
 }
 
+UserSchema.pre('save', function (next)  {
+    var user = this;
+    if (user.isModified("password")) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
 
 var User = mongoose.model("User", UserSchema);
 module.exports = {
