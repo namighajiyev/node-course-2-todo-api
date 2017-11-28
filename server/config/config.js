28e874789b7f360229507ce392fe24f38c8ee34a
Namiq Haciyev <namiq886@gmail.com>
2017-11-28 20:15:37 +0400

    bugfix in config 2

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,8 @@
 var env = process.env.NODE_ENV || 'development';
 
-var config = require("./config.json")
-var envConfig = config[env];
 if (env == "development" || env == "test") {
-
+    var config = require("./config.json")
+    var envConfig = config[env];
     Object.keys(envConfig).forEach((key) => {
         process.env[key] = envConfig[key];
     });
