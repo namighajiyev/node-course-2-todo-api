bfb571c5f095741a9e0c933a0d51867d365ad35c
Namiq Haciyev <namiq886@gmail.com>
2017-11-28 18:28:34 +0400

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -315,6 +315,30 @@ describe("POST /users/login", () => {
     });
 });
 
+describe("DELETE /users/me/token", () => {
+    it("should remove auth token on logout", (done) => {
+        //DELETE /users/me/token
+        // Set x-auth equal to token
+        //200
+        request(app)
+            .delete("/users/me/token")
+            .set("x-auth", users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                User.findById(users[0]._id).then((user) => {
+                        expect(user.tokens.length).toBe(0);
+                        done();
+                    },
+                    (err) => {
+                        done(err)
+                    })
+            })
+    });
+});
+
 after(function () {
     mongoose.disconnect().then().catch(e => {
         console.log("Error at teardown closing mongoose", err);
